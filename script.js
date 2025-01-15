9d56b23b81e6d298a50fbec531b8d99dc40a2323
Bryan Ochoa <bochoa314@.com>
2025-01-15 01:23:11 -0500

    Added correct link to JS in header, Added DOM listener to wait for html to load

---
+++ script.js
@@ -0,0 +1,8 @@
+// Wait for the DOM to load before adding event listeners
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Select the form element
+    const form = document.getElementById('loveForm');
+
+
+});
